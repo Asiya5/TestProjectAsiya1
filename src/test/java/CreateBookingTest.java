489a78c782826dea3024a0e69242c00cc843876a
Asiya Temirkanova <asiyatemirkanova@MacBook-Air-Asiya.local>
2024-06-25 12:50:47 +0600

    TestProjectAsiya

--- src/test/java/CreateBookingTest.java
+++ src/test/java/CreateBookingTest.java
@@ -1,2 +1,56 @@
-package PACKAGE_NAME;public class CreateBookingTest {
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import org.testng.Assert;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class CreateBookingTest {
+
+    private static int bookingId;
+
+    @BeforeClass
+    public void setup() {
+        RestAssured.baseURI = "https://restful-booker.herokuapp.com";
+    }
+
+    @Test
+    public void testCreateBooking() {
+        String requestBody = """
+                {
+                    "firstname" : "Alina",
+                    "lastname" : "Aytbekova",
+                    "totalprice" : 111,
+                    "depositpaid" : true,
+                    "bookingdates" : {
+                        "checkin" : "2023-06-01",
+                        "checkout" : "2023-06-10"
+                    },
+                    "additionalneeds" : "Breakfast"
+                }
+                """;
+
+        Response response = RestAssured
+                .given()
+                .contentType(ContentType.JSON)
+                .body(requestBody)
+                .post("/booking");
+
+        response.then().statusCode(200);
+        bookingId = response.jsonPath().getInt("bookingid");
+        Assert.assertNotEquals(bookingId, 0);
+    }
+
+    @Test(dependsOnMethods = "testCreateBooking")
+    public void testGetBooking() {
+        Response response = RestAssured
+                .given()
+                .get("/booking/" + bookingId);
+
+        response.then().statusCode(200);
+        String firstname = response.jsonPath().getString("firstname");
+        String lastname = response.jsonPath().getString("lastname");
+        Assert.assertEquals(firstname, "Alina");
+        Assert.assertEquals(lastname, "Aytbekova");
+    }
 }
--- src/test/java/GetBookingIdsTest.java
+++ src/test/java/GetBookingIdsTest.java
@@ -1,2 +1,37 @@
-package PACKAGE_NAME;public class GetBookingIdsTest {
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import org.testng.Assert;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class GetBookingIdsTest {
+    @BeforeClass
+    public void setup() {
+        RestAssured.baseURI = "https://restful-booker.herokuapp.com";
+    }
+
+    @Test
+    public void testGetBookingIdsByName() {
+        Response response = RestAssured
+                .given()
+                .queryParam("firstname", "Jim")
+                .queryParam("lastname", "Brown")
+                .get("/booking");
+
+        response.then().statusCode(200);
+        Assert.assertTrue(response.jsonPath().getList("bookingid").size() > 0);
+    }
+
+    @Test
+    public void testGetBookingIdsByDate() {
+        Response response = RestAssured
+                .given()
+                .queryParam("checkin", "2023-06-01")
+                .queryParam("checkout", "2023-06-10")
+                .get("/booking");
+
+        response.then().statusCode(200);
+        Assert.assertTrue(response.jsonPath().getList("bookingid").size() > 0);
+    }
+
 }
